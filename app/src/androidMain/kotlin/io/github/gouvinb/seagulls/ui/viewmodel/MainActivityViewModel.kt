5cf9c385397d1fbf1fb409cbb616b36e8d6b2896
gouvinb <brunogouvinhas@gmail.com>
2024-03-19 16:28:02 +0100

    docs - Dev: add missing license headers to source files
    - Correct the absence of license headers in multiple Kotlin and XML files

--- app/src/androidMain/kotlin/io/github/gouvinb/seagulls/ui/viewmodel/MainActivityViewModel.kt
+++ app/src/androidMain/kotlin/io/github/gouvinb/seagulls/ui/viewmodel/MainActivityViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 Gouvinb
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.gouvinb.seagulls.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
@@ -10,14 +25,14 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
 class MainActivityViewModel(
-    userDataRepository: UserDataRepository
+    userDataRepository: UserDataRepository,
 ) : ViewModel() {
     val uiState: StateFlow<MainActivityUiState> = userDataRepository.userDataStream.map {
         MainActivityUiState.Success(it)
     }.stateIn(
         scope = viewModelScope,
         initialValue = MainActivityUiState.Loading,
-        started = SharingStarted.WhileSubscribed(5_000)
+        started = SharingStarted.WhileSubscribed(5_000),
     )
 }
 
